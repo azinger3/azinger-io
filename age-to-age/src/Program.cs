8c61c70bf6a41f1b4e42c8ea01e87571181c206d
Rob Azinger <azinger3@gmail.com>
2025-07-03 02:00:06 +0000

    docker working better

--- age-to-age/src/Program.cs
+++ age-to-age/src/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -10,8 +9,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
 
 var summaries = new[]
 {
